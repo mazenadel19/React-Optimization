3a2bf5e36b9e9ce29a2797cd7aff555211939eaf
Mazen <mazenadel19@gmail.com>
2021-07-06 17:41:29 +0200

    using ReactMEMO to avoid useless comp excution

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Button from './components/UI/Button/Button'
+import DemoOutput from './components/Demo/DemoOutput'
 
 function App() {
 	const [showParagaph, setShowParagaph] = useState(false)
@@ -9,12 +10,12 @@ function App() {
 		// setShowParagaph(!prevShowParagaph)
 		setShowParagaph(prevShowParagaph => !prevShowParagaph)
 	}
+	console.log('app re-excuted')
 
 	return (
 		<div className='App'>
 			<h1>hello!</h1>
-			{showParagaph && <p>this is new</p>}
-			{/* <p className={state ? 'hide' : ''}>another</p> */}
+			<DemoOutput show={false} />
 			<Button onClick={toggleParagraphHandler}>toggle</Button>
 		</div>
 	)
---
+++ src/components/Demo/DemoOutput.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DemoOutput = props => {
+	console.log('demo re-excuted')
+
+	return <p>{props.show ? 'this is new' : ''}</p>
+}
+
+export default React.memo(DemoOutput)
