62bb169fae3548131a634421341ea3874dfa45ef
Mazen <mazenadel19@gmail.com>
2021-07-06 17:19:51 +0200

    toggle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import Button from './components/UI/Button/Button'
 
 function App() {
-  return (
-    <div className="App">
-      hello!
-    </div>
-  );
+	const [showParagaph, setShowParagaph] = useState(false)
+
+	const toggleParagraphHandler = () => {
+		// setShowParagaph(!prevShowParagaph)
+		setShowParagaph(prevShowParagaph => !prevShowParagaph)
+	}
+
+	return (
+		<div className='App'>
+			<h1>hello!</h1>
+			{showParagaph && <p>this is new</p>}
+			{/* <p className={state ? 'hide' : ''}>another</p> */}
+			<Button onClick={toggleParagraphHandler}>toggle</Button>
+		</div>
+	)
 }
 
-export default App;
+export default App
